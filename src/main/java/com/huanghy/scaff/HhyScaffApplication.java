ffaeac7f311f8e6aa1827016bafd50ddaec5b709
hhy <251152685@qq.com>
2021-02-28 16:12:35 +0800

    init

---
+++ src/main/java/com/huanghy/scaff/HhyScaffApplication.java
@@ -0,0 +1,13 @@
+package com.huanghy.scaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhyScaffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhyScaffApplication.class, args);
+    }
+
+}
