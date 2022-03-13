13836f475819de29f0f429ba9cc8283faf0aa790
hongyits <251152685@qq.com>
2022-03-13 13:37:11 +0800

    新增pc端脚本

---
+++ src/test/java/com/huanghy/scaff/Test1.java
@@ -0,0 +1,24 @@
+package com.huanghy.scaff;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+/**
+ * TODO
+ *
+ * @author HuangHy
+ * @date 2022/3/3 上午11:27
+ */
+
+public class Test1 {
+
+    @Test
+    public void tts(){
+        HashMap map = new HashMap();
+        map.put("12",null);
+
+
+
+    }
+}
